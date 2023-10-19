9522f8a9278a6bb66534e03369f92a69ad6221ac
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-19 13:28:23 -0400

    not working

--- main.ts
+++ main.ts
@@ -2,7 +2,31 @@
  *
  * Created by: Daki A.B
  * Created on: Oct 2023
- * This program ...
+ * This program compares two numbers
 */
 
+// variables
+
+let num1: number = randint(0, 99)
+let num2: number = randint(0,99)
+
+// on button A (show num1)
+
+input.onButtonPressed(Button.A, function () {
+  basic.showNumber(num1)
+})
+
+// on button B (show num2)
+
+input.onButtonPressed(Button.B, function () {
+  basic.showNumber(num2)
+})
+
+// comparing numbers
+
+input.onGesture(Gesture.Shake, function () {
+  if (num1 > num2)
+    basic.showNumber((num1 + basic.showString('>') (num2)
+
+})
 
