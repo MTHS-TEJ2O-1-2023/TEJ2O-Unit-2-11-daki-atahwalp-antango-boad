4c00c84433c5d931d531ea722f8f440130b490ce
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-19 13:10:03 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Daki A.B
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+
