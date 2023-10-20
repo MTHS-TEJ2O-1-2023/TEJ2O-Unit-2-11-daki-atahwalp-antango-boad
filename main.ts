cdf2a5c5ef37a5c508013f72e1058aa63088884a
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-20 13:02:03 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,18 +13,27 @@ let num2: number = randint(0,99)
 // on button A (show num1)
 
 input.onButtonPressed(Button.A, function () {
+  basic.showString('#1')
   basic.showNumber(num1)
+  basic.showIcon(IconNames.Happy)
+  basic.clearScreen()
 })
 
 // on button B (show num2)
 
 input.onButtonPressed(Button.B, function () {
+  basic.showString('#2')
   basic.showNumber(num2)
+  basic.showIcon(IconNames.Happy)
+  basic.clearScreen()
 })
 
 // comparing numbers
 
 input.onGesture(Gesture.Shake, function () {
-  if (num1 > num2)
-     basic.showString(num1 + '>', num2)
+  if (num1 < num2) {
+    basic.showString(num1 + '<' + num2)
+  } else {
+    basic.showString(num2 + '<' + num1)
+  }
 })
